ae5d4c600dfa100602cc5a2e53bf4b865a892d75
tjarker <tjark-petersen@gmx.de>
2021-03-08 11:15:49 +0100

    Update main.cpp

--- PlatformIOTest/src/main.cpp
+++ PlatformIOTest/src/main.cpp
@@ -1,45 +1,69 @@
-#include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
+#include <Arduino.h>
 
-const int led1 = 2;
+// define two tasks for Blink & AnalogRead
+void TaskBlink( void *pvParameters );
+void TaskAnalogRead( void *pvParameters );
 
+// the setup function runs once when you press reset or power the board
 void setup() {
-  pinMode(led1, OUTPUT);
-  Serial.begin(9600);
+
+  // Now set up two tasks to run independently.
+  xTaskCreate(
+    TaskBlink
+    ,  "Blink"   // A name just for humans
+    ,  128  // Stack size
+    ,  NULL
+    ,  2  // priority
+    ,  NULL );
+  xTaskCreate(
+    TaskAnalogRead
+    ,   "AnalogRead"
+    ,  128 // This stack size can be checked & adjusted by reading Highwater
+    ,  NULL
+    ,  1  // priority
+    ,  NULL );
+
+  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
 
-void toggleLED(void * parameter){
-  for(;;){
-    digitalWrite(led1, HIGH);
-    
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+void loop()
+{
+  // Empty. Things are done in Tasks.
+}
 
-    digitalWrite(led1, LOW);
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
 
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+void TaskBlink(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
+
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+
+  for (;;) // A Task shall never return or exit.
+  {
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    vTaskDelay( 100 / portTICK_PERIOD_MS ); // wait for one second
+    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    vTaskDelay( 900 / portTICK_PERIOD_MS ); // wait for one second
   }
 }
 
-void printTask(void * parameter){
-  Serial.println("Hello!");
-  vTaskDelay(500 / portTICK_PERIOD_MS);
-}
+void TaskAnalogRead(void *pvParameters)  // This is a task.
+{
+  (void) pvParameters;
 
-void loop() {
-  xTaskCreate(
-    toggleLED,    // Function that should be called
-    "Toggle LED",   // Name of the task (for debugging)
-    1000,            // Stack size (bytes)
-    NULL,            // Parameter to pass
-    1,               // Task priority
-    NULL             // Task handle
-  );
-  xTaskCreate(
-    printTask,
-    "print task",
-    1000,
-    NULL,
-    2,
-    NULL
-  );
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+
+  for (;;)
+  {
+    // read the input on analog pin 0:
+    // print out the value you read:
+    Serial.println("Hello");
+    vTaskDelay(3000/portTICK_PERIOD_MS);  // one tick delay (15ms) in between reads for stability
+  }
 }
