777744fdf6ab05d0c19600d386bb7ef14bbe1794
tjarker <tjark-petersen@gmx.de>
2021-04-23 08:34:04 +0200

    Change Can type

--- code/SolarVehicle/lib/BlackBox/logType.h
+++ code/SolarVehicle/lib/BlackBox/logType.h
@@ -2,30 +2,46 @@
 #include <ACAN.h>
 
 
-class logType {
+class StampedCANMessage {
     public:
-        uint8_t secs;
-        uint8_t mins;
-        uint8_t hours;
-        uint8_t days;
-        uint8_t months;
-        uint16_t years;
-        virtual const String toString() = 0;
-};
-
-class BBCANMessage : public logType {
-    public:
-        CANMessage msg;
+        uint16_t id;
+        uint8_t rtr;
+        uint8_t len;
+        union {
+            uint64_t data64        ; // Caution: subject to endianness
+            uint32_t data32 [2]    ; // Caution: subject to endianness
+            uint16_t data16 [4]    ; // Caution: subject to endianness
+            float    dataFloat [2] ; // Caution: subject to endianness
+            uint8_t  data   [8] = {0, 0, 0, 0, 0, 0, 0, 0} ;
+        };
+        uint8_t s;
+        uint8_t m;
+        uint8_t h;
+        uint8_t d;
+        uint8_t mon;
+        uint8_t y;
         
-        const String toString(){
-            char tmp[200];
-            snprintf(tmp,200,"\"%02d/%02d/%04d %02d-%02d-%02d\",%" PRIx32 ",%d,%d,%d,%" PRIu64 "",days,months,years,hours,mins,secs,msg.id,msg.ext,msg.rtr,msg.len,msg.data64);
-            return String(tmp);
-        }
-        uint32_t toString(char* buf){
-            snprintf(buf,200,"\"%02d/%02d/%04d %02d-%02d-%02d\",%" PRIx32 ",%d,%d,%d,%" PRIu64 "",days,months,years,hours,mins,secs,msg.id,msg.ext,msg.rtr,msg.len,msg.data64);
-        }
+    public: const String toString(){
+        char tmp[200];
+        snprintf(tmp,200,"\"%02d/%02d/%04d %02d-%02d-%02d\",%" PRIx32 ",%d,%d,%" PRIu64 "",
+        d,mon,y,h,m,s,id,rtr,len,data64);
+        return String(tmp);
+    }
+    public: uint32_t toString(char *buf){
+        snprintf(buf,200,"\"%02d/%02d/%04d %02d-%02d-%02d\",%" PRIx32 ",%d,%d,%" PRIu64 "",
+        d,mon,y,h,m,s,id,rtr,len,data64);
+    }
+
+    public: void stamp(){
+        s = second();
+        m = minute();
+        h = hour();
+        d = day();
+        mon = month();
+        y = year();
+    }
+
         static const String getHeader(){
-            return "\"time\",\"id\",\"ext\",\"rtr\",\"len\",\"data\"";
+            return "\"time\",\"id\",\"rtr\",\"len\",\"data\"";
         }
 };
