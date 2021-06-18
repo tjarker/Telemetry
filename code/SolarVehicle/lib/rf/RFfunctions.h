7694d0d21b3c960daa102737f977d5864005b1a2
tjarker <tjark-petersen@gmx.de>
2021-06-18 09:07:56 +0200

    let rf retry periodically

--- code/SolarVehicle/lib/rf/RFfunctions.h
+++ code/SolarVehicle/lib/rf/RFfunctions.h
@@ -22,7 +22,7 @@
 #define CSN_PIN 10      // SPI chip-select (CSN) pin
 #endif
 
-#define COUNT 0     // Number of transmission retries
+#define COUNT 2     // Number of transmission retries
 #define DELAY 5    // Delay between retries (= DELAY * 250 us + 250 us)
 
 #ifdef TEENSY40_BOARD
