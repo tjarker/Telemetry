d5b0a4fcd4fdbd8910313ca807145372c0e8827f
steffanmartin <s194006@student.dtu.dk>
2021-06-20 13:03:01 +0200

    tidy up code

--- code/SolarVehicle/src/telemetry/support-vehicle/SystemThds.h
+++ code/SolarVehicle/src/telemetry/support-vehicle/SystemThds.h
@@ -28,7 +28,7 @@ struct thdBundle
 THD_WORKING_AREA(WaReceiverThd, 2048);
 
 /*****************************************************
- * @brief   Thread function for receiverThd.      *
+ * @brief   Thread function for receiverThd.         *
  * @param   arg, typecast to Security class pointer. *
 ******************************************************/
 THD_FUNCTION(receiverThd, arg)
@@ -40,33 +40,30 @@ THD_FUNCTION(receiverThd, arg)
   chThdSleepMicroseconds(100);                                      // Sleep to allow for creation of secondary thread
 
   while (true){
-    bool flag = false; 
-    if (radio.available()){                                           // Check if TX node is transmitting data
-      if (sec->activate){
-        if (RFreceive(encrypted, BaseTelemetryMsg::length()<<1)){     // Check if message was received
-          sec->decrypt(encrypted, (uint8_t*)&received, 16);           // Decrypt received message
-          flag = true; 
-        } 
-      } else {
-        flag = RFreceive(&received, BaseTelemetryMsg::length()); 
-      }
-      if (flag){
+    if (radio.available()){                                         // Check if TX node is transmitting data
+      if (RFreceive(encrypted, BaseTelemetryMsg::length()<<1)){     // Check if message was received
+        if (sec->activate){                                         // Decrypt received message if encryption has been activated
+          sec->decrypt(encrypted, (uint8_t*)&received, 16);         
+        } else {
+          received.cmd = (cmd_t)encrypted[0];
+          for (int i = 1; i < 16; i++){
+            received.data[i] = (uint8_t)encrypted[i]; 
+          }  
+        }
         Serial.print("Received: ");
-        switch (received.cmd){                                        // Switch based on the message cmd field 
-          case RECEIVED_CAN:{                                         // Standard case, cmd matches RECEIVED_CAN
-            CanTelemetryMsg *ptr = (CanTelemetryMsg*)&received;       // Cast received to CanTelemetryMsg pointer
+        switch (received.cmd){                                      // Switch based on the message cmd field 
+          case RECEIVED_CAN:{                                       // Standard case, cmd matches RECEIVED_CAN
+            CanTelemetryMsg *ptr = (CanTelemetryMsg*)&received;     // Cast received to CanTelemetryMsg pointer
             ptr->toJSON(tempStr, sizeof(tempStr));                  
-            Serial.println(tempStr);                                  // Print message formatted as JSON
+            Serial.println(tempStr);                                // Print message formatted as JSON
           }
             break;
-          default:{                                                   // Default case, cmd does not match RECEIVED_CAN
+          default:{                                                 // Default case, cmd does not match RECEIVED_CAN
             received.toString(tempStr, sizeof(tempStr));            
-            Serial.println(tempStr);                                  // Print message formatted as string
+            Serial.println(tempStr);                                // Print message formatted as string
           }
             break;
         }
-      } else {
-        Serial.println("Could not receive message.");                 // Message was not received 
       }
     }
     chThdYield();                                                     // Yield for same-priority Thd
@@ -76,15 +73,16 @@ THD_FUNCTION(receiverThd, arg)
 // 2048 byte working stack for transmitterThd
 THD_WORKING_AREA(waTransmitterThd, 2048);
 
-/*****************************************************
- * @brief   Thread function for transmitterThd.   *
- * @param   arg, typecast to thdBundle pointer.   *
-******************************************************/
+/************************************************
+ * @brief   Thread function for transmitterThd. *
+ * @param   arg, typecast to thdBundle pointer. *
+*************************************************/
 THD_FUNCTION(transmitterThd, arg)
 {
-  thdBundle *transmitterBundle = (thdBundle*)arg;                       // Cast input argument to threadBundle pointer
+  thdBundle *transmitterBundle = (thdBundle*)arg;                       // Cast input argument to thdBundle pointer
   Security *transmitterSecurity = transmitterBundle->security;          // Extract bundle Security member variable
   Fifo<BaseTelemetryMsg> *transmitterFifo = transmitterBundle->fifo;    // Extract bundle Fifo member variable
+  BaseTelemetryMsg *message;                                            // BaseTelemetryMsg pointer to transmit
   uint16_t encrypted[16];                                               // Buffer to contain encrypted message
   char tempStr[128];                                                    // Buffer to print message to serial
 
@@ -93,36 +91,31 @@ THD_FUNCTION(transmitterThd, arg)
       Serial.readBytes((char*)transmitterFifo->tail(), 32);             // Read 32 bytes and cast them to a BaseTelemetryMessage                           
       transmitterFifo->moveTail();                                      // Increment fifo tail pointer
     }
-    if (!transmitterFifo->empty()){                                     // Check if there are messages in the fifo waiting to be sent
-      // Choose the message at the head of the fifo and encrypt it before transmission
-      (transmitterFifo->head())->toString(tempStr, sizeof(tempStr));
-      if (transmitterSecurity->activate){
-        transmitterSecurity->encrypt((uint8_t*)transmitterFifo->head(), encrypted, BaseTelemetryMsg::length());
-        if (RFtransmit(encrypted, BaseTelemetryMsg::length()<<1)){      // Check if message was transmitted succesfully 
-          Serial.print("Transmitted: "); 
-          Serial.println(tempStr);                                      // Print transmitted message to serial
-          transmitterFifo->moveHead();                                  // Increment fifo head pointer
-        } else {      
-          Serial.println("Transmission failed or timed out.");          // Message was not transmitted
-        } 
-      } else {
-        if (RFtransmit(transmitterFifo->head(), BaseTelemetryMsg::length())){
-            Serial.print("Transmitted: "); 
-            Serial.println(tempStr);                                    // Print transmitted message to serial
-            transmitterFifo->moveHead();                                // Increment fifo head pointer
-        } else {      
-          Serial.println("Transmission failed or timed out.");          // Message was not transmitted
+    if (!transmitterFifo->empty()){                                     // Check if there are messages to be sent in the buffer
+      message = transmitterFifo->head();                                // Get the next message from the head of the buffer
+      if (transmitterSecurity->activate){                               // Encrypt message if encryption is activated
+        transmitterSecurity->encrypt((uint8_t*)message, encrypted, BaseTelemetryMsg::length());
+      } else {                                                          // Else send the message as it is
+        encrypted[0] = (uint16_t)message->cmd; 
+        for (int i = 1; i < 16; i++){
+          encrypted[i] = (uint16_t)message->data[i]; 
         }
       }
-      if (transmitterFifo->head()->cmd == ENABLE_ENCRYPTION){
-        transmitterSecurity->activate = true; 
-        //radio.setPayloadSize(BaseTelemetryMsg::length()<<1); 
-      } else if (transmitterFifo->head()->cmd == DISABLE_ENCRYPTION){
-        transmitterSecurity->activate = false; 
-        //radio.setPayloadSize(BaseTelemetryMsg::length()); 
+      if (RFtransmit(encrypted, BaseTelemetryMsg::length()<<1)){        // Check if message was transmitted succesfully
+        message->toString(tempStr, sizeof(tempStr));
+        Serial.print("Transmitted: ");                                  // Message was transmitted
+        Serial.println(tempStr);                                        // Print transmitted message to serial
+        if (message->cmd == ENABLE_ENCRYPTION){                         // Check if message contained an ENABLE_ENCRYPTION cmd
+          transmitterSecurity->activate = true;                         // If so, activate encryption
+        } else if (message->cmd == DISABLE_ENCRYPTION){                 // Else, check if message contained a DISABLE_ENCRYPTION cmd
+          transmitterSecurity->activate = false;                        // If so, deactivate encryption
+        }
+        transmitterFifo->moveHead();                                    // Increment fifo head pointer
+      } else {
+        Serial.println("Transmission failed or timed out.");            // Message was not transmitted
       }
     }
-    chThdYield();                                                     // Yield for same-priority thread                       
+    chThdYield();                                                       // Yield for same-priority thread                       
   }
 } 
 
